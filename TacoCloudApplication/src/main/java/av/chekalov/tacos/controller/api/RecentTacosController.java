7e97053cda65c2848d5bf2a6c2ff1c22361b8431
Alexey Chekalov <alexejchekalov@yandex.ru>
2018-11-28 22:17:07 +0300

    Part6. Custom endpoints

---
+++ TacoCloudApplication/src/main/java/av/chekalov/tacos/controller/api/RecentTacosController.java
@@ -0,0 +1,40 @@
+package av.chekalov.tacos.controller.api;
+
+import av.chekalov.tacos.entity.Taco;
+import av.chekalov.tacos.repo.TacoRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.hateoas.Resources;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+@RepositoryRestController
+public class RecentTacosController {
+
+    private TacoRepository tacoRepository;
+
+    public RecentTacosController(TacoRepository tacoRepository) {
+        this.tacoRepository = tacoRepository;
+    }
+
+    @GetMapping(path = "/tacos/recent", produces = "application/hal+json")
+    public ResponseEntity<Resources<TacoResource>> recentTacos() {
+        PageRequest pageRequest = PageRequest.of(0, 12, Sort.by("createdAt").descending());
+        List<Taco> tacos = tacoRepository.findAll(pageRequest).getContent();
+
+        List<TacoResource> tacoResources = new TacoResourceAssembler().toResources(tacos);
+        Resources<TacoResource> recentResources = new Resources<>(tacoResources);
+
+        recentResources.add(
+                linkTo(methodOn(RecentTacosController.class).recentTacos()).withRel("recents"));
+        return new ResponseEntity<>(recentResources, HttpStatus.OK);
+
+    }
+}
