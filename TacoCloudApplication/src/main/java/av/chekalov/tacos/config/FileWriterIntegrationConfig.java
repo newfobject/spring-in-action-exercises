828719383eb9956cf7dde10bb319b11152a6ac96
Alexey Chekalov <alexejchekalov@yandex.ru>
2018-12-20 23:41:21 +0300

    Part9. Spring integration DSL configuration

--- TacoCloudApplication/src/main/java/av/chekalov/tacos/config/FileWriterIntegrationConfig.java
+++ TacoCloudApplication/src/main/java/av/chekalov/tacos/config/FileWriterIntegrationConfig.java
@@ -2,13 +2,11 @@ package av.chekalov.tacos.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.annotation.ServiceActivator;
-import org.springframework.integration.annotation.Transformer;
-import org.springframework.integration.channel.DirectChannel;
-import org.springframework.integration.file.FileWritingMessageHandler;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.channel.MessageChannels;
+import org.springframework.integration.file.dsl.Files;
 import org.springframework.integration.file.support.FileExistsMode;
-import org.springframework.integration.transformer.GenericTransformer;
-import org.springframework.messaging.MessageChannel;
 
 import java.io.File;
 
@@ -16,32 +14,15 @@ import java.io.File;
 public class FileWriterIntegrationConfig {
 
     @Bean
-    @Transformer(inputChannel = "textInChannel",
-            outputChannel = "fileWriterChannel")
-    public GenericTransformer<String, String> upperCaseTransformer() {
-        return String::toUpperCase;
+    public IntegrationFlow fileWriterFlow() {
+        return IntegrationFlows
+                .from(MessageChannels.direct("textInChannel"))
+                .<String, String>transform(String::toUpperCase)
+                .channel(MessageChannels.direct("fileWriterChannel"))
+                .handle(Files
+                        .outboundAdapter(new File("/tmp/spring-integration/files"))
+                        .fileExistsMode(FileExistsMode.APPEND)
+                        .appendNewLine(true))
+                .get();
     }
-
-    @Bean
-    @ServiceActivator(inputChannel = "fileWriterChannel")
-    public FileWritingMessageHandler fileWriter() {
-        FileWritingMessageHandler handler =
-                new FileWritingMessageHandler(
-                        new File("/tmp/spring-integration/files"));
-        handler.setExpectReply(false);
-        handler.setFileExistsMode(FileExistsMode.APPEND);
-        handler.setAppendNewLine(true);
-        return handler;
-    }
-
-    @Bean
-    public MessageChannel textInChannel() {
-        return new DirectChannel();
-    }
-
-    @Bean
-    public MessageChannel fileWriterChannel() {
-        return new DirectChannel();
-    }
-
 }
